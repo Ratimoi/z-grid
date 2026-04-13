6d5584218022ec2e9d9a6cedeb030ec098c86cf2
Ramiro Quevedo Paz <ramiroqpaz@gmail.com>
2026-04-13 11:50:17 -0300

    ✨ (main): Adiciona feature getch
    Implementa uma funcao que le uma tecla sem precisar confirmar com o enter

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+def getch():
+    import sys
+    import termios
+    import tty
+
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+
+    try:
+        tty.setraw(fd)
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+    return ch
+
 def mover_jogador(mapa, pos, comando):
     linha, coluna = pos
     mapa[linha][coluna] = '.'
