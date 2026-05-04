557c5eb32fe77c1368d9e31aa7b7dd118768ab1e
Ramiro Quevedo Paz <ramiroqpaz@gmail.com>
2026-05-04 19:34:39 -0300

    ✨ Refatora lógica de movimento
    Faz os zumbis se moverem de forma assincrona com o player

--- src/mapa.py
+++ src/mapa.py
@@ -1,9 +1,9 @@
 def criar_mapa():
     mapa = []
 
-    for linha in range(10):
+    for linha in range(15):
         mapa.append([])
-        for coluna in range(10):
+        for coluna in range(15):
             mapa[linha].append(".")
 
     return mapa
